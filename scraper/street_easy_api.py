9828b903c1e2c23a37b534edb1722dbfbc53784a
Robert West <robert.west@talkiatry.com>
2026-03-23 21:24:21 -0400

    one more ?

--- scraper/street_easy_api.py
+++ scraper/street_easy_api.py
@@ -67,7 +67,7 @@ def extract_listing_data(node: Dict[str, Any]) -> RentalListing:
     if lead_media and 'photo' in lead_media:
         photo = lead_media['photo']
         if photo:
-            photo_key = lead_media['photo'].get('key')
+            photo_key = lead_media['photo'].get('key', None)
             if photo_key:
                 # StreetEasy uses Zillow's CDN for images
                 listing['image_url'] = f"https://photos.zillowstatic.com/fp/{photo_key}-se_extra_large_1500_800.webp"
